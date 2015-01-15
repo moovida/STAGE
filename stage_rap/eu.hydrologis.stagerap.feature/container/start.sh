4072e252252913adc76fb8b9007233efd5f514ec
fb71 <falko@polymap.de>
2015-01-15 19:04:35 +0100

    add Linux32 and Win32 launchers and start script

---
+++ stage_rap/eu.hydrologis.stagerap.feature/container/start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -v
+
+# Simple start script
+#   - set working dir the INSTALLDIR (allow eclipse exe to find runtime)
+#   - find Java (hard wired)
+#   - find the workspace (hard wired)
+#   - define Java param and app params
+
+INSTALLDIR=`dirname $0`
+WORKSPACE=/home/falko/servers/virgo-nano-rap-3.7.0.CI-2014-11-24_03-37-00/
+PORT=10000
+JAVA_HOME=/home/falko/bin/jdk1.8
+
+cd $INSTALLDIR
+
+export VMARGS='-Xverify:none -server -XX:+TieredCompilation -Xmx1024m -XX:NewRatio=4 -XX:+UseG1GC -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=30 -XX:SoftRefLRUPolicyMSPerMB=1000'
+export ARGS='-console -consolelog -registryMultiLanguage'
+export LOGARGS='-Dorg.apache.commons.logging.Log=org.apache.commons.logging.impl.SimpleLog -Dorg.eclipse.equinox.http.jetty.log.stderr.threshold=info'
+
+echo $WORKSPACE
+./eclipse -vm $JAVA_HOME/bin/java $ARGS -vmargs $VMARGS -Dorg.osgi.service.http.port=$PORT -Dstage.workspace=$WORKSPACE $LOGARGS
--- stage_rap/eu.hydrologis.stagerap.feature/readme.md
+++ stage_rap/eu.hydrologis.stagerap.feature/readme.md
@@ -1,1 +1,17 @@
 This feature allows to deploy of STAGE plugins and dependencies.
+
+More info and docs:
+
+  - http://eclipse.org/equinox/documents/quickstart-framework.php
+  
+## Container
+
+  - the container subdir contains the OSGi launcher
+  - extracted from Eclipse-IDE install
+  - currently: Linux-32bit and Win-32bit 
+
+## Deploy
+
+  - copy container subdir to deploy/install dir
+  - export eu.hydrologis.stagerap.feature from Eclipse into the deploy dir
+  - Option -> Use class files compiled on workspace = true
